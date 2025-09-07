3c16581c2281b4d2a0a858fd8092062550b7465f
Mihail_Kurenysev <michaelkurenyshev@yandex.ru>
2025-09-08 00:35:33 +0300

    Финальное задание № 16 - откат до момента отправки на ревью

--- server/src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
+++ server/src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.booking.controller;
 
-import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
